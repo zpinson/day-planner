4bccb0070ef5c5640402b1816f27ff1bf928b69c
S Pinson <77454522+zpinson@users.noreply.github.com>
2021-02-11 20:44:33 -0500

    corrected the links to jquery in html. added the curent day in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,27 @@
 $(document).ready(function(){
-
+// $('.saveBtn').on('click', 
     // check for btn clicks insid an on click
-    $('.saveBtn').on('click', function(){
+    document.querySelectorAll('.saveBtn').onclick = function(){
         //grab my value for time and text
         var value = $(this).siblings('.task').val();
-        var time = $(this).parent().attr('id')
+        var time = $(this).parent().attr('#hour-9')
 
         //save them to local storage
-        localStorage.setItem()
-    })
+        localStorage.setItem('task', value)
+        localStorage.setItem('time', time)
+    }
+
+    var update = function () {
+        date = moment(new Date())
+        datetime.html(date.format('dddd, MMMM Do YYYY'));
+    };
+    
+    $(document).ready(function(){
+        datetime = $('#currentDay')
+        update();
+        setInterval(update, 1000);
+    });
+    
 
     //write a function to update the hour in the time blocks
     //get current time with moment
@@ -22,5 +35,5 @@ $(document).ready(function(){
 
 
     //load any saved data from local storage
-    $('#hour-9 .task').val(localStorage.getItem('hour-9'));
+    // $('#hour-9 .task').val(localStorage.getItem('hour-9'));
 })
