725f6729bcf442d398d8e4dcbddb850c496ef799
S Pinson <77454522+zpinson@users.noreply.github.com>
2021-02-14 17:21:47 -0500

    added a new dive with time in a 24 hour format. add a css to hide the new div. updated the for each loop to work. added the function to add the local storage to the website

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,67 @@
-// hourEl = document.querySelectorAll(".hour");
-// console.log(hourEl);
-// console.log("hour: hourEl", hourEl[0].textContent);
+//current time in a 24 hour format 
+var currentHour = moment().format("k");
+// current time stored as a number
+var currentHourVl = parseInt(currentHour);
+
+// pulling from the local storage and putting in the coresponding slot
+document.getElementById("hour-9").innerHTML = localStorage.getItem("9AM");
+document.getElementById("hour-10").innerHTML = localStorage.getItem("10AM");
+document.getElementById("hour-11").innerHTML = localStorage.getItem("11AM");
+document.getElementById("hour-12").innerHTML = localStorage.getItem("12PM");
+document.getElementById("hour-13").innerHTML = localStorage.getItem("1PM");
+document.getElementById("hour-14").innerHTML = localStorage.getItem("2PM");
+document.getElementById("hour-15").innerHTML = localStorage.getItem("3PM");
+document.getElementById("hour-16").innerHTML = localStorage.getItem("4PM");
+document.getElementById("hour-17").innerHTML = localStorage.getItem("5PM");
+document.getElementById("hour-18").innerHTML = localStorage.getItem("6PM");
 
 $(document).ready(function () {
-  //
-  // check for btn clicks insid an on click
-  // document.querySelector('.saveBtn').onclick =
+  //saving to the local storage the data as value and corresponding hour as the key with the saving button
   $(".saveBtn").on("click", function () {
     //grab my value for time and text
     var value = $(this).siblings(".task").val();
     var time = $(this).siblings(".hour").text();
-    // var time = $(this).parent().attr('id').text();
-    console.log(time);
+    // console.log(time);
 
     //save them to local storage
     localStorage.setItem(time, value);
-    // localStorage.setItem('time', time)
   });
-
+  //grabs today date from moment.js
   var update = function () {
     date = moment(new Date());
     datetime.html(date.format("dddd, MMMM Do YYYY"));
-    var currentHour = moment().format("hA"); 
-    var currentHourVl = parseInt(currentHour.slice(0, -2))
-    console.log(currentHourVl)
-  };
 
+    // console.log(currentHourVl);
+  };
+  // insert the curebt date to the html
   $(document).ready(function () {
     datetime = $("#currentDay");
     update();
     setInterval(update, 1000);
   });
 
-  $(".hour").each(function () {
-    console.log($(this).text());
-    var hourVl = parseInt($(this).text().slice(0, -2))
-    
-    // if ($(this).text() === currentHour)
-    console.log(hourVl)
+  //com
+  $(".time").each(function () {
+    // console.log($(this).text());
+    var hourVl = parseInt($(this).text());
+
+    if (hourVl === currentHourVl) {
+      $(this).siblings(".task").addClass("present");
+      $(this).siblings(".task").removeClass("past");
+      $(this).siblings(".task").removeClass("future");
+    } else if (hourVl > currentHourVl) {
+      $(this).siblings(".task").addClass("future");
+      $(this).siblings(".task").removeClass("past");
+      $(this).siblings(".task").removeClass("present");
+    } else {
+      $(this).siblings(".task").addClass("past");
+      $(this).siblings(".task").removeClass("present");
+      $(this).siblings(".task").removeClass("future");
+    }
+    // console.log(hourVl);
   });
+
+ 
 
   //write a function to update the hour in the time blocks
   //get current time with moment
@@ -46,13 +69,12 @@ $(document).ready(function () {
   //loop over out time block hint: (look up each)
 
   //check if current time has passed - add class
-  for (var i = 0; i < hourEl.length; i++) {
-    const element = array[index];
-  }
+  
   // addClass('past)
   //check if it is the current time - remove class add class
   // future time - remove class remove class add class
 
   //load any saved data from local storage
-  // $('#hour-9 .task').val(localStorage.getItem('hour-9'));
+//   document.getElementById("123").innerHTML = localStorage.getItem("9AM");
+  //   $('#hour-9 .task').val(localStorage.getItem('9AM'));
 });
